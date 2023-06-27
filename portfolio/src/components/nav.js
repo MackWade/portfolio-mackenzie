09f06af8ea6939afc08164bcb9f2848b48e95bef
MackWade <kinkinwade@gmail.com>
2023-06-27 09:24:22 -0700

    switching machines

--- portfolio/src/components/nav.js
+++ portfolio/src/components/nav.js
@@ -1,7 +1,26 @@
+import React from 'react'
+
 export default function Nav() {
+
+    /*
+        The following menu items represents the contents of the page that can be accessed through the menu
+     */
+    const menus = [
+        { name: "Home" },
+        { name: "Projects" },
+        { name: "About" },
+        { name: "Contact Me" },
+    ]
+
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        /*
+            The Nav bar is established within this div
+                * fixed is used to position an element relative to the browser window
+                * w - means the width
+                * h - means the height
+         */
+        <div className={'fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#83b1ff]'}>
+            <div></div>
+        </div>
     )
 }
