f826a35c6fe735ccd94fbc0155d112e2a12379b4
MackWade <kinkinwade@gmail.com>
2023-07-13 21:41:42 -0700

    nav/home revised

--- portfolio/src/components/home.js
+++ portfolio/src/components/home.js
@@ -1,43 +1,41 @@
 import React, {useState} from 'react'
+import { Element } from 'react-scroll'
 import img2 from '../images/Astrowballoons.jpg'
 import img1 from "../images/astronaut.png";
 import { HiArrowNarrowRight } from 'react-icons/hi';
 
 export default function Home() {
     return (
-        <div className={' w-full h-[100vh] flex justify-between items-center px-4 bg-[#83b1ff]'}>
 
-            <div className='max-w-[1000px] mx-auto px-3 flex flex-col justify-center h-full text-[#333333]'>
-                <p className='text-4xl sm:text-4xl '>Hi, my name is</p>
-                <h1 className='text-4xl sm:text-7xl font-bold text-[#FFFFFF]'>
-                    Mackenzie Wade
-                </h1>
-                <h2 className='text-4xl sm:text-7xl font-bold text-[#555555]'>
-                    Full Stack Developer.
-                </h2>
-                <p className='text-[#555555] py-4 max-w-[700px]'>
-                    I’m a full-stack developer looking for a Fall 2023 co-op. Currently, focused on
-                    building responsive crypto managing application using C++ and R.
-                </p>
-                <div className={'flex space-x-6' }>
-                    <button className='text-white group border-2 px-6 py-3 my-2 flex items-center '>
-                        Linkedin
-                        <span className='group-hover:rotate-90 duration-300'>
+            <div className={' w-full h-[100vh] flex justify-between items-center px-4 bg-[#83b1ff]' } id = "home"  >
 
-                        </span>
-                    </button>
-                    <button className='text-white group border-2 px-6 py-3 my-2 flex items-center'>
-                        Resume
-                        <span className='group-hover:rotate-90 duration-300'>
+                <div className='max-w-[1000px] mx-auto px-3 flex flex-col justify-center h-full text-[#381A46]'>
+                    <p className='text-4xl sm:text-4xl '>Hi, my name is</p>
+                    <h1 className='text-4xl sm:text-7xl font-bold text-[#FFFFFF]'>
+                        Mackenzie Wade
+                    </h1>
+                    <h2 className='text-4xl sm:text-7xl font-bold text-[#381A46]'>
+                        Full Stack Developer.
+                    </h2>
+                    <p className='text-[#381A46] py-4 max-w-[700px]'>
+                        I’m a full-stack developer looking for a Fall 2023 co-op. Currently, focused on
+                        building responsive crypto managing application using C++ and R.
+                    </p>
+                    <div className={'flex space-x-6' }>
+                        <a href="https://www.linkedin.com/in/mackenziew8/" target="_blank" rel="noopener noreferrer">
+                            <button  className='text-white  hover:border-[#381A46] hover:text-[#381A46] group border-2 px-6 py-3 my-2 flex items-center '>
+                                Linkedin
 
-                        </span>
-                    </button>
+                            </button>
+                        </a>
+
+                    </div>
+                </div>
+                <div>
+                    {/* Creates the image*/}
+                    <img className={'h-[50vh]'} src={img2} alt={""} />
                 </div>
             </div>
-            <div>
-                {/* Creates the image*/}
-                <img className={'h-[50vh]'} src={img2} alt={""} />
-            </div>
-        </div>
+
     )
 }
--- portfolio/src/components/nav.js
+++ portfolio/src/components/nav.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useRef} from 'react'
 import img1 from '../images/astronaut.png'
 import {
     FaBars,
@@ -9,19 +9,24 @@ import {
     FaLinkedinIn,
 } from 'react-icons/fa';
 import { Link } from 'react-scroll';
+import Scroll from 'react-scroll'
+const ScrollLink = Scroll.ScrollLink
 export default function Nav() {
 
     const [nav, setNav] = useState(false);
+    const [click,setClick] = useState(false)
+
     const handleClick = () => setNav(!nav);
+
 
     /*
         The following menu items represents the contents of the page that can be accessed through the menu
      */
     const menus = [
-        { name: "Home" },
-        { name: "Projects" },
-        { name: "About" },
-        { name: "Contact Me" },
+        { name: "Home", ref:"home" },
+        { name: "Projects", ref:"" },
+        { name: "About", ref: ""},
+        { name: "Contact Me", ref: "" },
     ]
 
     return (
@@ -41,7 +46,11 @@ export default function Nav() {
             <div>
                 {/***Normal Screen Menu****/}
                 <ul  className='hidden md:flex space-x-6 '>
-                    {menus.map(({ name }) => <li className=' font-bold py-6 text-2xl text-[#FFFFFF] '>{name}</li>)}
+                    {menus.map(({ name,ref }) => <li className=' font-bold py-6 text-2xl text-[#FFFFFF] hover:underline underline-offset-8 decoration-[#381A46]'>
+                        <Link to={ref} spy={true} smooth={true} offset={-100} durration={500} >
+                            {name}
+                        </Link>
+                    </li>)}
                 </ul>
                 {/***Mobile Screen Menu****/}
                 <div onClick={handleClick} className='md:hidden z-10'>
@@ -49,9 +58,9 @@ export default function Nav() {
                 </div>
 
                 <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#444444] flex flex-col justify-center items-center'}>
-                    {menus.map(({ name }) => <li className='py-6 text-4xl' >
-                        <Link onClick={handleClick} to='skills' smooth={true} duration={500}>
-                        {name}
+                    {menus.map(({ name,ref }) => <li className='py-6 text-4xl' >
+                        <Link to={ref} spy={true} smooth={true} offset={-100} durration={500} >
+                            {name}
                         </Link>
                     </li>)}
                 </ul>
